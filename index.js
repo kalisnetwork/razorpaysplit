79ce6a2d1c2748d0cfa99aaae3476d003f2367e0
kalisnetwork <kalyankodisha@gmail.com>
2025-01-09 18:51:05 +0530

    Port Update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/payments', paymentRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`server run on http://localhost:${PORT}`);
